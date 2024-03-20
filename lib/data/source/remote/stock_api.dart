3c1c36258e7c9922ddf28aa13f9a1c6f73d0f5ef
HyungwonJin <hwjin92@gmail.com>
2024-03-21 08:24:57 +0900

    Update .gitignore and pubspec.yaml, add CompanyListing model and StockApi class

---
+++ lib/data/source/remote/stock_api.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class StockApi {
+  static const String baseUrl = 'https://www.alphavantage.co/';
+  final apiKey = dotenv.env['APIKEY'];
+
+  final http.Client client;
+
+  StockApi({required this.client});
+
+  Future<http.Response> getListings(String apikey) async {
+    return await client.get(
+      Uri.parse(
+        'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=$apikey',
+      ),
+    );
+  }
+}
