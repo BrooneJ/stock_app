9dd9d7ce5a9a0ae26a883ba73755e7671cbf9558
HyungwonJin <hwjin92@gmail.com>
2024-03-22 08:55:29 +0900

    Add CompanyInfoDto and CompanyInfo models

--- lib/data/source/remote/stock_api.dart
+++ lib/data/source/remote/stock_api.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:stock_app/data/source/remote/dto/company_info_dto.dart';
+import 'package:stock_app/domain/model/company_info.dart';
 
 class StockApi {
-  static const String baseUrl = 'https://www.alphavantage.co/';
+  static const String baseUrl = 'https://www.alphavantage.co';
   final apiKey = dotenv.env['APIKEY'];
 
   final http.Client _client;
@@ -13,8 +17,22 @@ class StockApi {
     final apikey = apiKey ?? "";
     return await _client.get(
       Uri.parse(
-        'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=$apikey',
+        '$baseUrl/query?function=LISTING_STATUS&apikey=$apikey',
       ),
     );
+  }
+
+  Future<CompanyInfoDto> getCompanyInfo({
+    required String symbol,
+    String? apikey,
+  }) async {
+    final apikey = apiKey ?? "";
+    final response = await _client.get(
+      Uri.parse(
+        '$baseUrl/query?function=OVERVIEW&symbol=$symbol&apikey=$apikey',
+      ),
+    );
+
+    return CompanyInfoDto.fromJson(jsonDecode(response.body));
   }
 }
